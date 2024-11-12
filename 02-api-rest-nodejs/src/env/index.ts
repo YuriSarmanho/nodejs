a7a8ef6ef83cc43ced1516fa8ad63421da83dc21
yuri.sarmanho <yuri.sarmanho@prodepa.pa.gov.br>
2024-11-12 16:20:41 -0300

    testes

--- 02-api-rest-nodejs/src/env/index.ts
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test', override: true })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
@@ -10,9 +16,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error('Warning Invalid enviroment variables', _env.error.format())
+  console.error('Warning Invalid enviroment variables' + _env.error.format())
 
-  throw new Error('Invalid enviroment variables')
+  throw new Error('Invalid enviroment variables' + _env.error.format())
 }
 
 export const env = _env.data
---
+++ 02-api-rest-nodejs/test/transactions.spec.ts
@@ -0,0 +1,54 @@
+import { it, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
+import { execSync } from 'node:child_process'
+import request from 'supertest'
+import { app } from '../src/app'
+
+describe('transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:roolback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction ', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it.todo('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')!
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transaction).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 5000,
+      }),
+    ])
+  })
+})
