4141c48792860a9c7853e18b2e673ed659348a0f
yuriSarmanho <yuri.sarmanho@hotmail.com>
2025-04-21 20:28:18 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Comentar na resposta

---
+++ 04-clean-ddd/src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryAnswersRepository } from 'tests/repository/in-memory-answers-repository'
+import { MakeAnswers } from 'tests/factories/make-answers'
+import { InMemoryAnswerCommentsRepository } from 'tests/repository/in-memory-answer-comment-repository'
+import { CommentOnAnswerUseCase } from './comment-on-answer'
+
+let inMemoryAnswerRepository: InMemoryAnswersRepository
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: CommentOnAnswerUseCase
+
+describe('Comment On Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswersRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswerRepository,
+      inMemoryAnswerCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on answer', async () => {
+    const answer = MakeAnswers()
+
+    await inMemoryAnswerRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: answer.authorId.toString(),
+      content: 'Comentario teste',
+    })
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      'Comentario teste',
+    )
+  })
+})
