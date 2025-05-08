93dd957d0089d8e54f2fbabf266d0f81aff1a9a8
yuri.sarmanho@prodepa.pa.gov.br <yuri.sarmanho@hotmail.com>
2025-05-07 21:02:14 -0300

    Criando Pipe de validação do Zod

--- 05-nest-clean/src/controllers/create-account.controller.ts
+++ 05-nest-clean/src/controllers/create-account.controller.ts
@@ -1,14 +1,25 @@
-import { ConflictException } from "@nestjs/common";
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, ConflictException, HttpCode, UsePipes } from "@nestjs/common";
 import { hash } from "bcryptjs";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
-  async handle(@Body() body: any){
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema){
     const { name, email, password } = body
 
     const userWithTheSameEmail = await this.prisma.user.findUnique({
