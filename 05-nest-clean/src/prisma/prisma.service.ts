97d47878bd9f88696d021bbd5c1c8c37b57c686f
yuri.sarmanho@prodepa.pa.gov.br <yuri.sarmanho@hotmail.com>
2025-06-24 12:34:49 -0300

    node-05

--- 05-nest-clean/src/prisma/prisma.service.ts
+++ 05-nest-clean/src/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import {
   Injectable,
-  type OnModuleDestroy,
-  type OnModuleInit,
+  OnModuleDestroy,
+  OnModuleInit,
 } from '@nestjs/common'
 import { PrismaClient } from 'generated/prisma'
 
---
+++ 05-nest-clean/test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import { execSync } from 'child_process'
+import { randomUUID } from 'crypto'
+import 'dotenv/config'
+
+import { PrismaService } from '@/prisma/prisma.service'
+
+const prisma = new PrismaService()
+
+
+function generateUniqueDatabaseUrl( schemaId: string) {
+  if(!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL enviroment variable.')
+  }
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseUrl(schemaId)
+
+  process.env.DATABASE_URL  = databaseURL
+
+  execSync('pnpm prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
