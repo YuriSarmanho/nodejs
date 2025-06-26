064fc5c62f45338e9a08d667c157a68c8db149c9
yuri.sarmanho <yuri.sarmanho@prodepa.pa.gov.br>
2025-06-26 10:38:16 -0300

    Testes E2E de perguntas

--- 05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
+++ 05-nest-clean/src/controllers/fetch-recent-questions.controller.ts
@@ -4,34 +4,34 @@ import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { PrismaService } from '@/prisma/prisma.service'
 import { z } from 'zod'
 
-const pageQueryParamSchema = z.string().optional().default('1').transform(Number).pipe(
-  z.number().min(1)
-)
-
-type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
 
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
-  constructor(
-    private prisma: PrismaService
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
   @Get()
-  async handle(
-    @Query('page', queryValidationPipe) page: PageQueryParamSchema
-  ) {
-    const perPage = 1
+  async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
+    const perPage = 20
 
     const questions = await this.prisma.question.findMany({
-      take: 1,
+      take: perPage,
       skip: (page - 1) * perPage,
       orderBy: {
-        createdAt: 'desc'
-    },
-  })
-  return { questions }  
-}
+        createdAt: 'desc',
+      },
+    })
+
+    return { questions }
+  }
 }
