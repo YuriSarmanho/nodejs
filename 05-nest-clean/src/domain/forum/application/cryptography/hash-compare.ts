fab3161a61f6adc1994d0d9eed9592c5d18c6c62
yuri.sarmanho <yuri.sarmanho@prodepa.pa.gov.br>
2025-08-05 16:22:41 -0300

    Casos de Uso: Autenticação e Cadastro

--- 05-nest-clean/src/domain/forum/application/cryptography/hash-compare.ts
+++ 05-nest-clean/src/domain/forum/application/cryptography/hash-compare.ts
@@ -1,3 +1,3 @@
-export abstract class Hasher {
-  abstract compare(plain: string): Promise<boolean>
+export abstract class HashCompare {
+  abstract compare(plain: string, hash: string): Promise<boolean>
 }
