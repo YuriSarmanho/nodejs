a08fe0eb0cbf86956d20b04b1cba64ba798478be
yuri.sarmanho@prodepa.pa.gov.br <yuri.sarmanho@hotmail.com>
2025-12-20 19:54:05 -0300

    Controller: Responder Pergunta

--- 05-nest-clean/src/domain/forum/application/use-cases/answer-question.ts
+++ 05-nest-clean/src/domain/forum/application/use-cases/answer-question.ts
@@ -4,11 +4,12 @@ import { AnswersRepository } from '../repositories/answers-repository'
 import { right, type Either } from '@/core/either'
 import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
 import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
+import { Injectable } from '@nestjs/common'
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: string
+  authorId: string
   questionId: string
-  attachmentsIds: string[]
+  attachmentIds: string[]
   content: string
 }
 type AnswerQuestionUseCaseResponse = Either<
@@ -18,22 +19,23 @@ type AnswerQuestionUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class AnswerQuestionUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
   async execute({
-    instructorId,
+    authorId,
     questionId,
     content,
-    attachmentsIds,
+    attachmentIds,
   }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
-      authorId: new UniqueEntityID(instructorId),
+      authorId: new UniqueEntityID(authorId),
       questionId: new UniqueEntityID(questionId),
     })
 
-    const answerAttachments = attachmentsIds.map((attachmentId) => {
+    const answerAttachments = attachmentIds.map((attachmentId) => {
       return AnswerAttachment.create({
         attachmentId: new UniqueEntityID(attachmentId),
         answerId: answer.id,
